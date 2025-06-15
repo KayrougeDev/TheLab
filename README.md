435888325f9935b5313601708a65c25cde08e9a4
KayrougeDev <litrib@proton.me>
2025-06-15 21:16:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+The Lab
+=======
+
+The Lab is a story mod, multiplayer is disabled for the moment, and you cannot create a new world, only join the one pre-created.
+
+Screenshot:
+==========
+TODO
+
+Help:
+==========
+WIP
---
+++ src/main/java/fr/kayrouge/thelab/TheLabClient.java
@@ -0,0 +1,18 @@
+package fr.kayrouge.thelab;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = TheLab.MODID, dist = Dist.CLIENT)
+public class TheLabClient {
+    public TheLabClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
